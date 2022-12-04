104b66adcf30d6620b05306c694275727a8ba3fa
Ivan <csgo.dr@mail.ru>
2022-12-04 20:38:04 +0700

    problem solving

--- FinalTestWork/Program.cs
+++ FinalTestWork/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿string[] FormsAnArrayThreeLength(string[] arrayInput)
+{
+    string[] resultArray = new string[arrayInput.Length];
+    for (int i = 0; i < arrayInput.Length; i++)
+    {
+        int length = arrayInput[i].Length;
+        if (length <= 3)
+        {
+            resultArray[i] = arrayInput[i];
+        }
+    }
+    return resultArray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+}
+
+string[] array = {"hello","2","world","^-^","Russia"};
+PrintArray(
+        FormsAnArrayThreeLength(array));
